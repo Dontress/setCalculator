4e45ec9cb2f901f341c2cf26ce16a37fbb4bcf9a
Petr Šíma <45285946+Dontress@users.noreply.github.com>
2021-11-17 14:31:08 +0100

    Update setcal.c

--- setcal.c
+++ setcal.c
@@ -4,23 +4,28 @@
 #include <string.h>
 
 #define MAXLEN 1024
+#define ITEMLEN 30
 
 // gcc -std=c99 -Wall -Wextra -Werror setcal.c -o setcal
 // ./setcal FILE
 
 typedef struct{
-    char* items;
+    char *items;
     int cardinality;
 }set_t;
 
 void read_next_line(char* array, FILE* file);
 bool is_last_line(char* file_line);
+void string_to_set(set_t* set, char* file_line, int line_number);
+bool set_constructor(set_t* set, int cardinality);
 
 int main(int argc, char* argv[]){
 
+    int line_number = 0;
     char file_name[10] = {'f', 'i', 'l', 'e', '.', 't', 'x', 't'}; 
     char file_line[MAXLEN];
     bool lastLine = false;
+    set_t set[10];
 
     FILE* file;
     file = fopen( file_name, "r" );
@@ -29,11 +34,20 @@ int main(int argc, char* argv[]){
         return -1;
     }
     
+    // hlavni do-while pro cteni souboru po radku
     do
     {
         read_next_line( file_line, file);
         printf("%s", file_line);
 
+        if( file_line[0] == 'U' || file_line[0] == 'S' ){
+            string_to_set( &set[line_number] ,file_line, line_number);
+        }
+            
+        printf("%d\n", set[line_number].cardinality);
+
+
+        line_number++;
     } while ( is_last_line( file_line ) == false );
     
 
@@ -41,16 +55,25 @@ fclose(file);
 return 0;    
 }
 
-bool set_constructor(set_t* set, int cardinality){
-    set->cardinality = cardinality;
+void string_to_set(set_t* set, char* file_line, int line_number){
 
-    set->items = malloc(sizeof(char) * cardinality);
-    if( set->items == NULL ){
-        fprintf(stderr, "set allocation failed");
-        return 0;
+    int number_of_items = 0;
+
+    for (int i = 0; file_line[i] != '\0'; i++)
+    {
+        if( file_line[i] == ' ' )
+            number_of_items++;
     }
 
-return 1;
+    set->cardinality = number_of_items;
+
+    set->items = malloc(sizeof(char) * number_of_items * ITEMLEN);
+    if(set->items == NULL){
+        fprintf(stderr, "set allocation failed");
+    }
+
+   
+   
 }
 
 void read_next_line(char* file_line, FILE* file){
