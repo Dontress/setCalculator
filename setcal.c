a0e4fe9e5036c5f118ce5e59fa0d5dd26be10bdf
Libor129 <46523630+Libor129@users.noreply.github.com>
2021-11-30 02:00:33 +0100

    Add files via upload

--- setcal.c
+++ setcal.c
@@ -8,6 +8,7 @@
 #define MAXLINES 1000
 #define NUM_OF_COMMANDS 19
 #define COMMAND_MAXLEN 15
+#define SET_ID_MAXLEN 4
 
 // gcc -std=c99 -Wall -Wextra -Werror setcal.c -o setcal
 // ./setcal FILE
@@ -22,6 +23,7 @@ typedef struct{
 }universum_t;
 
 typedef struct{
+    char set_or_rel;
     int *set;
     int *size_of_elem_arr;
     int cardinality;
@@ -37,19 +39,19 @@ void fill_universum_items(universum_t *set, char *line_string);
 void print_universum(universum_t universum);
 set_t make_set(char *line_string, universum_t u);
 void print_set(set_t set, universum_t u, int number_of_set);
-//void print_command_result(char *line_string, set_t* set, char commands[NUM_OF_COMMANDS][COMMAND_MAXLEN]);
+void print_command_result(char *line_string, set_t* set, universum_t u, char commands[NUM_OF_COMMANDS][COMMAND_MAXLEN]);
+int load_set_id(char *line_string, int *pos_after_command);
+void domain(set_t set, universum_t u);
 
 int main(){
 
     char line_string[MAXLEN] = "";
-    /**
     char commands[NUM_OF_COMMANDS][COMMAND_MAXLEN] = {
         "empty", "card", "complement", "union", "intersect",
         "minus", "subseteq", "subset", "equals", "reflexive",
         "symmetric", "antisymmetric", "transitive", "function", "domain",
         "codomain", "injective", "surjective", "bijective"
     };
-    */
     int sets_in_file = 0;
     int commands_in_file = 0;
 
@@ -78,8 +80,8 @@ int main(){
 
     rewind(file);
 
-    // alokuj pamet pro pole setu
-    set = malloc(sets_in_file*sizeof(set_t));
+    // alokuj pamet pro pole setu 
+    set = malloc((sets_in_file + 1)*sizeof(set_t)); // sets_in_file + 1 = sets_in_file + univerzum
     if( set == NULL ){
         fprintf(stderr, "chyba alokace pole setu");
         return -1;
@@ -94,11 +96,13 @@ int main(){
     if( read_universum( universum, line_string ) ){
         return -1;
     }
-    printf("universum: ");
-    print_universum( *universum );
-    printf("\n");
+    //printf("universum: ");
+    //print_universum( *universum );
+    //printf("\n");
     
-    for (int i = 0; i < sets_in_file; i++)
+    rewind(file);
+
+    for (int i = 0; i < sets_in_file + 1; i++)
     {
         read_line( line_string, file );
         set[i] = make_set(line_string, *universum);
@@ -107,17 +111,17 @@ int main(){
 
     // cteni a provadeni prikazu
     while (read_line( line_string, file )) {
-        
+
         commands_in_file++;
         if ((sets_in_file + commands_in_file) > (MAXLINES - 1)) {
             fprintf(stderr, "presazen podporovany pocet radku");
             return -1;
         }
 
-        //print_command_result(line_string, set, commands);
+        print_command_result(line_string, set, *universum, commands);
     }
     
-    for (int i = 0; i < sets_in_file; i++)
+    for (int i = 0; i < sets_in_file + 1; i++)
     {
         free(set[i].set);
         free(set[i].size_of_elem_arr);
@@ -278,6 +282,13 @@ void print_universum(universum_t universum){
 // vytvori set ci mnozinu (pracujeme s tim stejne)
 set_t make_set(char *line_string, universum_t u){
     set_t set;
+
+    if ((line_string[0] == 'S') || (line_string[0] == 'R')) {
+        set.set_or_rel = line_string[0];
+    } else if (line_string[0] == 'U') {
+        set.set_or_rel = 'S';
+    }
+
     set.cardinality = count_elems( line_string );
 
     int char_count = 0;
@@ -299,7 +310,7 @@ set_t make_set(char *line_string, universum_t u){
         {
             set.size_of_elem_arr[i] -= 1;
             if( set.cardinality % 2  ){
-                fprintf(stderr, "neplatne zadana relace");  // osetreni uplnosti relace (ma sudy pocet prvku => kazdy prvek je ve dvojci)
+                fprintf(stderr, "neplatne zadana relace\n");  // osetreni uplnosti relace (ma sudy pocet prvku => kazdy prvek je ve dvojci)
                 exit( 1 );
             }
         }
@@ -319,12 +330,12 @@ set_t make_set(char *line_string, universum_t u){
     {
         streak = 0;
         k = 0;
-
+        /**
         if(j == u.cardinality && j != 1){
             fprintf(stderr, "chybne zadani mnoziny ci relace\n");
             exit( 1 );
         }   
-
+        */
         for (j = 0; j < u.cardinality; j++)
         {
             if(line_string[i] == u.items[j][k]){
@@ -357,11 +368,23 @@ return set;
 // vypise mnozinu ci relaci
 void print_set(set_t set, universum_t u, int number_of_set){
     printf("set %d is: \n", number_of_set);
+    if (number_of_set == 0) {
+        printf("U ");
+    } else {
+        printf("%c ", set.set_or_rel);
+    }
+
     for (int i = 0; i < set.cardinality; i++)
     {
+        if (set.set_or_rel == 'R' && (i % 2 == 0)) {
+            printf("(");
+        }
         for (int j = 0; j < set.size_of_elem_arr[i]; j++)
         {
             printf("%c", u.items[ set.set[i] ][ j ]);
+        }
+        if (set.set_or_rel == 'R' && (i % 2 == 1)) {
+            printf(")");
         }
         printf(" ");
     }
@@ -369,30 +392,171 @@ void print_set(set_t set, universum_t u, int number_of_set){
 }
 
 // precti prikaz a zavolej funkci pro provedeni prikazu
-/**
-void print_command_result(char *line_string, set_t* set, char commands[NUM_OF_COMMANDS][COMMAND_MAXLEN]) {
+void print_command_result(char *line_string, set_t* set, universum_t u, char commands[NUM_OF_COMMANDS][COMMAND_MAXLEN]) {
 
-    if (line_string[0] != 'C' && line_string[1] != ' ') {
-        fprintf(stderr, "chybne zadany prikaz");
+    if (line_string[0] != 'C' || line_string[1] != ' ' ||  line_string[2] < 'a' || line_string[2] > 'z') {
+        fprintf(stderr, "chybne zadany prikaz\n");
         exit( 1 );
     }
 
-    char cur_command[COMMAND_MAXLEN];
+    // zjisti nazev prikazu
+    char cur_command[COMMAND_MAXLEN] = "";
+    int pos_after_command = -1;
     for (int i = 0; i < COMMAND_MAXLEN; i++) {
         if (line_string[i + 2] == ' ') {
+            pos_after_command = (i + 2);
             break;
         }
         cur_command[i] = line_string[i + 2];
     }
-    printf("%s\n", cur_command); // vymazat
 
+    // vrat id prikazu z pole prikazu, pokud nenajdes, vypis chybu
+    int cur_command_id = -1;
     for (int i = 0; i < NUM_OF_COMMANDS; i++) {
         if (strcmp( cur_command, commands[i] ) == 0) {
-            printf ("%d\n", i);
+            cur_command_id = i;
+            break;
+        }
+    }
+    if (cur_command_id == -1) {
+        fprintf(stderr, "prikaz neexistuje\n");
+        exit( 1 );
+    }
+
+    // zjisti, jestli je za prikazem zadane id setu
+    int *pos_after_command_ptr = &pos_after_command;
+    int command_set_id_1 = load_set_id(line_string, pos_after_command_ptr);
+
+    // na zaklade zadaneho prikazu zkontroluj, jestli je zadany spravny pocet mnozin ci relaci
+    // a over, jestli se skutecne jedna o mnozinu ci relaci
+    if (cur_command_id >= 0 && cur_command_id <= 2) {     // empty - complement: 1 mnozina na vstupu
+        if (line_string[ pos_after_command ] != '\n') {
+            fprintf(stderr, "chybne zadany prikaz\n");
+            exit( 1 );
+        }
+        if (set[ command_set_id_1 ].set_or_rel != 'S') {
+            fprintf(stderr, "prikaz ocekaval 1 argument typu mnozina\n");
+            exit( 1 );
+        }
+
+        // sem funkce
+
+    } else if (cur_command_id >= 3 && cur_command_id <= 8) {     // union - equals: 2 mnoziny na vstupu
+        int command_set_id_2 = load_set_id(line_string, pos_after_command_ptr);
+
+        if (line_string[ pos_after_command ] != '\n') {
+            fprintf(stderr, "chybne zadany prikaz\n");
+            exit( 1 );
+        }
+        if (set[ command_set_id_1 ].set_or_rel != 'S' || set[ command_set_id_2 ].set_or_rel != 'S') {
+            fprintf(stderr, "prikaz ocekaval 2 argumenty typu mnozina\n");
+            exit( 1 );
+        }
+
+        // sem funkce
+
+    } else if (cur_command_id >= 9 && cur_command_id <= 15) {    // reflexive - codomain: 1 relace na vstupu
+        if (line_string[ pos_after_command ] != '\n') {
+            fprintf(stderr, "chybne zadany prikaz\n");
+            exit( 1 );
+        }
+        if (set[ command_set_id_1 ].set_or_rel != 'R') {
+            fprintf(stderr, "prikaz ocekaval 1 argument typu relace\n");
+            exit( 1 );
+        }
+
+        // sem funkce
+        // priklad:
+        switch (cur_command_id) {
+            case 14:
+                domain( set[ command_set_id_1 ], u);
+                break;
+        }
+
+    } else if (cur_command_id >= 16 && cur_command_id <= 18) {     // injective - bijective: 1 relace a 2 mnoziny
+        int command_set_id_2 = load_set_id(line_string, pos_after_command_ptr);
+        int command_set_id_3 = load_set_id(line_string, pos_after_command_ptr);
+
+        if (line_string[ pos_after_command ] != '\n') {
+            fprintf(stderr, "chybne zadany prikaz\n");
+            exit( 1 );
+        }
+        if (set[ command_set_id_1 ].set_or_rel != 'R' ||
+            set[ command_set_id_2 ].set_or_rel != 'S' ||
+            set[ command_set_id_3 ].set_or_rel != 'S') {
+            fprintf(stderr, "prikaz ocekaval prvni argument typu relace a dalsi 2 argumenty typu mnozina\n");
+            exit( 1 );
+        }
+
+        // sem funkce
+
+    }
+
+}
+
+// nacte id mnoziny u prikazu
+int load_set_id(char *line_string, int *pos_after_command) {
+
+    int position = *pos_after_command;
+    char command_set[SET_ID_MAXLEN] = "";
+    int command_set_id = 0;
+
+    position++;
+    if ( line_string[ position ] == ' ' || line_string[ position ] == '\n' || line_string[ position ] == '\0') {
+            fprintf(stderr, "u prikazu chybi zadana mnozina\n");
+            exit( 1 );
+    }
+    for (int i = 0; i < SET_ID_MAXLEN; i++) {
+        if ( line_string[ position ] >= '0' && line_string[ position ] <= '9' ) {
+            command_set[i] = line_string[ position ];
+        } else if ( line_string[ position ] == ' ' || line_string[ position ] == '\n' ) {
+            command_set_id = atoi(command_set);
+            break;
+        } else {
+            fprintf(stderr, "u prikazu byl zadan neciselny identifikator mnoziny\n");
+            exit( 1 );
+        }
+        position++;
+    }
+    *pos_after_command = position;
+
+    if (atoi(command_set) > 1000 || atoi(command_set) < 1) {
+        fprintf(stderr, "u prikazu byl zadan neplatny identifikator mnoziny\n");
+        exit( 1 );
+    }
+
+    return (command_set_id - 1); // odecitame 1, protoze v argumentech se indexuje od 1
+}
+
+// 14 - definicni obor relace
+void domain(set_t set, universum_t u) {
+
+    // usporadej prvni prvky dvojic do prvni poloviny pole
+    for (int i = 0; i < (set.cardinality / 2); i++) {
+        set.set[i] = set.set[2*i];
+    }
+    set.cardinality = (set.cardinality / 2);
+
+    // oznac opakujici se prvky ktere nebudes tisknout
+    for (int i = 0; i < set.cardinality; i++) {
+        for (int j = (i + 1); j < set.cardinality; j++) {
+            if ( set.set[i] == set.set[j]) {
+                set.set[j] = -1;
+            }
         }
     }
 
-    printf("%d\n", set[2].set[2]);
-
+    // tiskni vysledek
+    printf("S ");
+    for (int i = 0; i < set.cardinality; i++) {
+        if (set.set[i] == -1) {
+            continue;
+        }
+        for (int j = 0; j < u.size_of_elem_arr[ set.set[i] ]; j++) {
+            printf("%c", u.items[ set.set[i] ][ j ]);
+        }
+        printf(" ");
+    }
+    printf("\n");
+    
 }
-*/
